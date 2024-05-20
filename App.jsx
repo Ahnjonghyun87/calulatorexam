334cc2b850a83cf4209d93f4a90cb6957bcaec19
Ahnjonghyun87 <167166874+Ahnjonghyun87@users.noreply.github.com>
2024-05-20 16:07:12 +0900

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,30 @@
+export default function App() {
+  const onClick = () => {
+    const onPlusNumber = () => {
+      return setNumber();
+    };
+
+    const onMinusNumber = () => {
+      return;
+    };
+  };
+
+  const [plus, setPluse] = useState(0);
+  const [minus, setMinus] = useState(0);
+  return (
+    <div>
+      <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
+      <div>
+        <input type="number" /> 만큼을{" "}
+        <button onClick={() => onPlusNumber()}>더할게요</button>{" "}
+        <button onClick={() => onMinusNumber()}>뺄게요</button>
+        <button>초기화</button>
+      </div>
+      <hr />
+      <div>
+        <h3>결과</h3>
+        <p>0</p>
+      </div>
+    </div>
+  );
+}
